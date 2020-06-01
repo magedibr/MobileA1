d9be27e3fb95ed045e88be863f6d0c62ae1473bb
magedibr <59849099+magedibr@users.noreply.github.com>
2020-05-31 21:37:17 -0400

    1

--- js/magedIbr.js
+++ js/magedIbr.js
@@ -1,13 +1,36 @@
 $(document).ready(function() {
     console.log("in doc ready");
 
+    //Student Variables
     const name = "Abdelrahman";
     const ID = 991343504;
     const Login = "magedibr";
 
+    //Template Constants
+    let headMessage=`A1 for : ${name}`;
+    let footMessage = `Student ID: ${ID} / Login: ${Login}`;
 
-    // Start-up formating
-    $("header").html("A1 for : "+ name);
+    // Header formatting
+    $("header").html(headMessage);
+    $("header").addClass("hStyle");
+
+    //Footer formatting
+    $(".item4").html(footMessage);
+
+
+    //Data class
+    class MyData{
+
+        constructor(initial,element ,image ) {
+
+            this.intial=initial;
+            this.element=element;
+            this.image= image;
+        }
+    }
+
+    let myData = new Array();
+    myData.push(new MyData("M","Mom","Dohs"));
 
 
 

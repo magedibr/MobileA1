44b72dfc50f88741247408e9c0275ccc0f324ba9
magedibr <59849099+magedibr@users.noreply.github.com>
2020-05-31 20:12:27 -0400

    1

---
+++ js/magedIbr.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    console.log("in doc ready");
+
+    const name = "Abdelrahman";
+    const ID = 991343504;
+    const Login = "magedibr";
+
+
+    // Start-up formating
+    $("header").html("A1 for : "+ name);
+
+
+
+
+
+
+
+
+
+
+})

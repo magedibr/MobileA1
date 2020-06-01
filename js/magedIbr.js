0cfd779e5da9c9aafb82f21b69904a820631deb7
magedibr <59849099+magedibr@users.noreply.github.com>
2020-05-31 23:09:03 -0400

    1

--- js/magedIbr.js
+++ js/magedIbr.js
@@ -6,16 +6,20 @@ $(document).ready(function() {
     const ID = 991343504;
     const Login = "magedibr";
 
+
     //Template Constants
     let headMessage=`A1 for : ${name}`;
     let footMessage = `Student ID: ${ID} / Login: ${Login}`;
+
 
     // Header formatting
     $("header").html(headMessage);
     $("header").addClass("hStyle");
 
+
     //Footer formatting
     $(".item4").html(footMessage);
+    $(".item4").css("text-align","center");
 
 
     //Data class
@@ -29,13 +33,25 @@ $(document).ready(function() {
         }
     }
 
+
     // make an array with the objects, use for if to make list, add the image?
 
     let myData = new Array();
-    myData.push(new MyData("M","Mom","Dohs"));
+    myData.push(new MyData("M","Milk","/images/milk.jpeg"));
+    myData.push(new MyData("A","Apple","/images/apple.jpeg"));
+    myData.push(new MyData("G","Goat","/images/goat.jpeg"));
+    myData.push(new MyData("E","Ear","/images/ear.jpeg"));
+    myData.push(new MyData("D","Dog","/images/dog.jpeg"));
+    myData.push(new MyData("I","Iris","/images/iris.jpeg"));
+    myData.push(new MyData("B","Book","/images/book.jpeg"));
+    myData.push(new MyData("R","Racoon","/images/racoon.jpeg"));
 
 
 
+
+        for(let i of myData){
+            $(".item2").html(i);
+        }
 
 
 
